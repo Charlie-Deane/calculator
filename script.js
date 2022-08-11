b76ef66c5b1eb817a010ba53852d45879966d1bc
Charlie Deane <charlie@homerow.dev>
2022-08-10 23:03:22 -0400

    functions arrayToNumber and numberToArray are functional, still need to work on edge cases for operate()

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+
 //converts an array to a numerical value
 function arrayToNumber(arr)
 {
-    let str = null;
+    let str = '0';
     for(let i = 0; i < arr.length; i++)
     {
         str += arr[i];
@@ -12,18 +13,58 @@ function arrayToNumber(arr)
 //converts a numerical value to an array of characters
 function numberToArray(num)
 {
+    let arr = ['0','0','0','0','0','0','0','0','0','0'];
+    if(num > 9999999999)
+    {
+        return arr;
+    }
+
+    let numString = num.toString();
+    let n = 0
+    for(let i = (10 - numString.length); i < 10; i++)
+    {
+        arr[i] = numString[n];
+        n++;
+    }
+    return arr;
 }
 
+//operates with array a, string operand, and array b
 function operate(a, operand, b)
 {
-    if(typeof a == "array")
-    {
-        a = arrayToNumber(a);
-    }
+    //cast variable types
+    a = arrayToNumber(a);
+    console.log(a);
     b = arrayToNumber(b);
+    console.log(b);
 
-    
+    //create resultant pointer
+    let c;
+
+    //switch for actual operation
+    switch(operand)
+    {
+        case '+':
+            c = a + b;
+            break;
+        case '-':
+            c = a - b;
+            break;
+        case '/':
+            //check for divide by 0 error
+            if(b == 0) 
+            {
+                return ['e','r','r','o','r','E','r','r','o','r']
+            }
+            c = a / b;
+            break;
+        case '*':
+            c = a * b;
+            break;
+    }
+    return numberToArray(c);
 }
+
 //gather screen variables
 const digit_0 = document.getElementById("digit_0");
 const digit_1 = document.getElementById("digit_1");
@@ -53,3 +94,22 @@ const numpad_decimal = document.getElementById("numpad_decimal");
 const super_equals = document.getElementById("super_equals");
 const super_clear = document.getElementById("super_clear");
 const super_backspace = document.getElementById("super_backspace"); 
+
+let a = 24;
+let aArr = numberToArray(a);
+a = arrayToNumber(aArr);
+console.log(aArr)
+
+let b = 22;
+let bArr = numberToArray(b);
+b = arrayToNumber(bArr);
+console.log(bArr);
+
+let c = '-';
+console.log(c);
+
+console.log(operate(aArr,c,bArr));
+
+//note to self: you still need to fix division!
+//note to self: you need to fix negative numbers on subtraction!
+
